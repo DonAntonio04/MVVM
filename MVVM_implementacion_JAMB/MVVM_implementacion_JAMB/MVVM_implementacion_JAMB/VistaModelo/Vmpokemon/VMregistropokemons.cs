0b6a1621b322116571f0397c30b2a3d89151e288
DonAntonio04 <berrellezantonio04@gmail.com>
2023-11-13 07:51:51 -0700

    nuevo

--- MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/VistaModelo/Vmpokemon/VMregistropokemons.cs
+++ MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/VistaModelo/Vmpokemon/VMregistropokemons.cs
@@ -11,23 +11,53 @@ namespace MVVM_implementacion_JAMB.VistaModelo.Vmpokemon
     {
         
             #region VARIABLES
-            string _Texto;
-            #endregion
-            #region CONSTRUCTOR
-            public VMregistropokemons(INavigation navigation)
+             string _Txtcolorfondo;
+             string _Txtcolorpoder;
+             string _Txtnombre;
+             string _Txtnro;
+             string _Txtpoder;
+             string _Txticono;
+        #endregion
+        #region CONSTRUCTOR
+        public VMregistropokemons(INavigation navigation)
             {
              Navigation = navigation;
             }
-            #endregion
-            #region OBJETOS
-            public string Texto
+        #endregion
+        #region OBJETOS
+        public string Txtcolorfondo
             {
-                get { return _Texto; }
-                set { SetValue(ref _Texto, value); }
+               get { return _Txtcolorfondo; }
+                set { SetValue(ref _Txtcolorfondo, value); }
             }
-            #endregion
-            #region PROCESOS
-            public async Task ProcesoAsyncrono()
+        public string Txtcolorpoder
+        {
+            get { return _Txtcolorpoder; }
+            set { SetValue(ref _Txtcolorpoder, value); }
+        }
+        public string Txtnombre
+        {
+            get { return _Txtnombre; }
+            set { SetValue(ref _Txtnombre, value); }
+        }
+        public string Txtnro
+        {
+            get { return _Txtnro; }
+            set { SetValue(ref _Txtnro, value); }
+        }
+        public string Txtpoder
+        {
+            get { return _Txtpoder; }
+            set { SetValue(ref _Txtpoder, value); }
+        }
+        public string Txticono
+        {
+            get { return _Txticono; }
+            set { SetValue(ref _Txticono, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
             {
 
             }
--- MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/Vistas/Pokemon/Registrarpokemon.xaml.cs
+++ MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/MVVM_implementacion_JAMB/Vistas/Pokemon/Registrarpokemon.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVM_implementacion_JAMB.VistaModelo.Vmpokemon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace MVVM_implementacion_JAMB.Vistas.Pokemon
         public Registrarpokemon()
         {
             InitializeComponent();
+            BindingContext = new VMregistropokemons(Navigation);
         }
     }
-}
+} 
